3a1702e4bd039a4ae95c96567a70a5c51bf6b99e
20099267 <kevin9000m@Gmail.com>
2023-03-30 15:06:35 +0100

    changes ui design and adds buttons for numbers

--- src/main/java/com/example/javacalculator/HelloController.java
+++ src/main/java/com/example/javacalculator/HelloController.java
@@ -2,47 +2,171 @@ package com.example.javacalculator;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
-    private TextField operand1TextField;
+
+    private String operator = "";
+    private double operand1 = 0;
+    private double operand2 = 0;
 
     @FXML
-    private TextField operand2TextField;
+    private Button add;
+
+    @FXML
+    private Button clear;
+
+    @FXML
+    private Button divide;
+
+    @FXML
+    private Button equals;
+
+    @FXML
+    private Button multiply;
+
+    @FXML
+    private Button nmb0;
+
+    @FXML
+    private Button nmb1;
+
+    @FXML
+    private Button nmb2;
+
+    @FXML
+    private Button nmb3;
+
+    @FXML
+    private Button nmb4;
+
+    @FXML
+    private Button nmb5;
+
+    @FXML
+    private Button nmb6;
+
+    @FXML
+    private Button nmb7;
+
+    @FXML
+    private Button nmb8;
+
+    @FXML
+    private Button nmb9;
+
+    @FXML
+    private TextField operand1TextField;
 
     @FXML
     private Label resultLabel;
 
     @FXML
-    private void add(ActionEvent event) {
-        double operand1 = Double.parseDouble(operand1TextField.getText());
-        double operand2 = Double.parseDouble(operand2TextField.getText());
-        double result = operand1 + operand2;
-        resultLabel.setText(Double.toString(result));
+    private Button subtract;
+
+    @FXML
+    void add(ActionEvent event) {
+        operator = "+";
+        operand1 = Double.parseDouble(operand1TextField.getText());
+        operand1TextField.setText("");
     }
 
     @FXML
-    private void subtract(ActionEvent event) {
-        double operand1 = Double.parseDouble(operand1TextField.getText());
-        double operand2 = Double.parseDouble(operand2TextField.getText());
-        double result = operand1 - operand2;
-        resultLabel.setText(Double.toString(result));
+    void clear(ActionEvent event) {
+        operand1TextField.setText("");
+        resultLabel.setText("");
+        operand1 = 0;
+        operand2 = 0;
+        operator = "";
     }
 
     @FXML
-    private void multiply(ActionEvent event) {
-        double operand1 = Double.parseDouble(operand1TextField.getText());
-        double operand2 = Double.parseDouble(operand2TextField.getText());
-        double result = operand1 * operand2;
-        resultLabel.setText(Double.toString(result));
+    void divide(ActionEvent event) {
+        operator = "/";
+        operand1 = Double.parseDouble(operand1TextField.getText());
+        operand1TextField.setText("");
     }
+
     @FXML
-    private void divide(ActionEvent event) {
-//        double operand1 = Double.parseDouble(operand1TextField.getText());
-//        double operand2 = Double.parseDouble(operand2TextField.getText());
-//        double result = operand1 * operand2;
-//        resultLabel.setText(Double.toString(result));
+    void equals(ActionEvent event) {
+        operand2 = Double.parseDouble(operand1TextField.getText());
+        double result = 0;
+        switch(operator) {
+            case "+":
+                result = operand1 + operand2;
+                break;
+            case "-":
+                result = operand1 - operand2;
+                break;
+            case "*":
+                result = operand1 * operand2;
+                break;
+            case "/":
+                result = operand1 / operand2;
+                break;
+        }
+        resultLabel.setText(String.valueOf(result));
+        operand1TextField.setText(String.valueOf(result));
+        operand1 = result;
+        operand2 = 0;
+    }
+
+    @FXML
+    void multiply(ActionEvent event) {
+        operator = "*";
+        operand1 = Double.parseDouble(operand1TextField.getText());
+        operand1TextField.setText("");
+    }
+
+    @FXML
+    void nmb0(ActionEvent event) {
+        operand1TextField.setText(operand1TextField.getText() + "0");
+    }
+
+    @FXML
+    void nmb1(ActionEvent event) {
+        operand1TextField.setText(operand1TextField.getText() + "1");
+    }
+
+    @FXML
+    void nmb2(ActionEvent event) {
+        operand1TextField.setText(operand1TextField.getText() + "2");
+    }
+
+    @FXML
+    void nmb3(ActionEvent event) {
+        operand1TextField.setText(operand1TextField.getText() + "3");
+    }
+
+    @FXML
+    void nmb4(ActionEvent event) {
+        operand1TextField.setText(operand1TextField.getText() + "4");
+    }
+
+    @FXML
+    void nmb5(ActionEvent event) {
+        operand1TextField.setText(operand1TextField.getText() + "5");
+    }
+
+    @FXML
+    void nmb6(ActionEvent event) {
+        operand1TextField.setText(operand1TextField.getText() + "
     }
 }
+
+    @FXML
+    void nmb7(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nmb8(ActionEvent event) {
+
+    }
+
+    @FXML
+    void nmb9(ActionEvent event) {
+
+    }
